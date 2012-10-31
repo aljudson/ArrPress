f16e4a1b47ee7681cbfb7b4cfa095118fb0d36b5
unknown <Ajudson@HAM-AJUDSON.sanimax.int>
2012-10-31 14:55:29 -0400

    initial push (rails skeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArrPress::Application.config.secret_token = 'e5a1b3412d144393e10b97de73b6352f80c110c8382a57722a506079ba7a9e5721b7c42285b673a7b241b443d50f597a306d5162a37a870d2f5f474bc039f8ef'
